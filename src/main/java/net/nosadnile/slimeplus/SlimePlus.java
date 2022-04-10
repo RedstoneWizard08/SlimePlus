170eb294b568c2f3666ac5391cbc16fa44a7d819
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-04-10 01:06:45 +0000

    Fix CI and logging

--- src/main/java/net/nosadnile/slimeplus/SlimePlus.java
+++ src/main/java/net/nosadnile/slimeplus/SlimePlus.java
@@ -4,19 +4,32 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import net.nosadnile.slimeplus.items.ItemController;
+import net.nosadnile.slimeplus.util.MessageUtil;
 
 public class SlimePlus extends JavaPlugin implements SlimefunAddon {
     public static SlimePlus INSTANCE;
 
     @Override
     public void onEnable() {
+        // Set instance
         INSTANCE = this;
+
+        // Log init
+        MessageUtil.log("&2[&aSlimePlus&2] &aEnabled!");
+
+        // Register items
+        MessageUtil.log("&2[&aSlimePlus&2] &9Registering items...");
         ItemController.registerAllItems(this);
+        MessageUtil.log("&2[&aSlimePlus&2] &9Registered items!");
     }
 
     @Override
     public void onDisable() {
+        // Unset instance
         INSTANCE = null;
+
+        // Log deinit
+        MessageUtil.log("&2[&aSlimePlus&2] &cDisabled!");
     }
 
     @Override
